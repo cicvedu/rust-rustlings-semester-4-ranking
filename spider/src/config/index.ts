dee9c7033e118c970ffc402f5a580737a9dce08e
ZhiyuanSue <2262387848@qq.com>
2024-03-10 09:40:55 +0800

    update classroom

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -4,7 +4,7 @@ export const config = dotenv.config();
 
 export const organiztion: string = 'cicvedu';            // 组织
 export const fullOrganization: string = '119796606-cicv-rust-classroom'; // classroom名称
-export const assignment = 'rust-rustlings';   // assignment
+export const assignment = 'rustlings-semester-3';   // assignment
 export const works = ['main']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 export const SESSION_TOKEN = process.env['SESSION_TOKEN'];
